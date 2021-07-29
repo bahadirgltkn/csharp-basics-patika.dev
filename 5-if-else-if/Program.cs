07f677a094edde437a3dbd66374bbbfd16d3423c
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-07-29 19:58:51 +0300

    Adding if-else statements

---
+++ 5-if-else-if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5_if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+
+            if(time >= 6 && time < 11)
+                Console.WriteLine("Good Morning");
+            if(time <= 18)
+                Console.WriteLine("have a good day");
+            else    
+                Console.WriteLine("Good night");
+
+            // Ternary Operator
+            // variable = (condition) ? expressionTrue :  expressionFalse;
+            //string result = time <= 18 ? "have a good day" : "good night";
+            string result = time >=6 && time < 11 ? "good morning" : time <= 18 ? "hava a good day" : "good night";
+            Console.WriteLine(result);
+        }
+    }
+}
