28cf937d14efa5219f87fd97eaed6df073d11c1a
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-07-30 00:43:15 +0300

    Adding while and foreach

---
+++ 8-while-foreach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _8_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WHILE
+            int  sum = 0, i = 1;
+            Console.Write("please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            while (i <= number)
+            {
+                sum += i;
+                i++;
+            }
+            Console.WriteLine("Average of numbers: " + sum / number);
+
+            Console.WriteLine("***** FOREACH *****");
+            //FOREACH
+            int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine("item: " + num);
+            }
+
+
+        }
+    }
+}
