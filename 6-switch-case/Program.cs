3bd1c7f06e733d179840a05036133f1d8fe3b3aa
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-07-29 23:16:32 +0300

    Adding switch-case

---
+++ 6-switch-case/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _6_switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            // expression
+            switch (month)
+            {
+                case 1: 
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+                default:
+                    Console.WriteLine("Wrong data");
+                    break;
+            }
+
+
+            switch (month)
+            {
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("You are in summer");
+                    break;
+            }
+        }
+    }
+}
