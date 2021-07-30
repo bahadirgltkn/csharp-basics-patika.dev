a6884e77df526b6a3b1e45ee1714c9a8fe346079
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-07-30 11:10:52 +0300

    Adding arrays

---
+++ 9-arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _9_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = new string[5];
+            string[] animals = {"cat","dog","bird","monkey"};
+
+            int[] numbers;
+            numbers = new int[5];
+
+            colors[0] = "red";
+            numbers[3] = 20;
+
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(animals[2]);
+
+            Console.Write("enter the length of the array: ");
+            int len = int.Parse(Console.ReadLine());
+            int[] numbers2 = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("{0}. number: ", i+1);
+                numbers2[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            foreach (var item in numbers2)
+                sum += item;
+
+            Console.WriteLine("Average: " + sum / len);
+
+        }
+    }
+}
