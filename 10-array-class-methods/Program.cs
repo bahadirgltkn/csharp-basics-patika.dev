5d4e7dacc2083c67d7faa098a7cba30ff360f0c7
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-07-30 15:14:15 +0300

    Adding array-class-methods

---
+++ 10-array-class-methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _10_array_class_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {23,12,4,86,14,17,87,3};
+
+            Console.WriteLine("*****UNSORTED*****");
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+            
+
+            Console.WriteLine("*****SORTED*****");
+            Array.Sort(numbers);
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+            
+
+            //CLEAR  Clear(Array array, int index, int length)
+            // arrays = {1,2,3,4,5,6} --> arrays.Clear(arrays,2,2) --> output: 1 2 0 0 5 6
+            Console.WriteLine("*****Array Clear*****");
+            Array.Clear(numbers,2,2);
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+            
+
+            //Reverse
+            Console.WriteLine("*****Array Reverse*****");
+            Array.Reverse(numbers);
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+            
+            //IndexOf
+            Console.WriteLine("*****Array IndexOf*****");
+            Console.WriteLine(Array.IndexOf(numbers,86));
+
+            //Resize
+            Console.WriteLine("*****Array Resize*****");
+            Console.WriteLine("old length: " + numbers.Length);
+            Array.Resize<int>(ref numbers, 10);
+            Console.WriteLine("new length: " + numbers.Length);
+            
+
+
+        }
+    }
+}
