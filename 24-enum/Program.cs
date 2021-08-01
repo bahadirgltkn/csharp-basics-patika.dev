831569eeac012c5f0f7c2cca2d036e5fc231b0a3
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-08-01 23:07:01 +0300

    Adding enum

---
+++ 24-enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _24_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enumeration
+            Console.WriteLine(Days.Monday);
+            Console.WriteLine((int)Days.Sunday);
+
+
+            // enums increase code readability
+            int temparature = 32;
+            if(temparature <= (int)Weather.Normal)
+                Console.WriteLine("Wait");
+            else if(temparature >= (int)Weather.Hot)
+                Console.WriteLine("a hot day");
+            
+        }
+    }
+
+    enum Days
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday=23,
+        Saturday,
+        Sunday
+    }
+
+    enum Weather
+    {
+        Cold = 5,
+        Normal = 20,
+        Hot = 25,
+        VeryHot =30
+    }
+}
