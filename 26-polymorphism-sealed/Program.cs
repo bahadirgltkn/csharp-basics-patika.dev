36371738ade08fb33e0424f701199f483359231a
Bahadır GÜLTEKİN <63556574+bahadirgltkn@users.noreply.github.com>
2021-08-02 15:41:21 +0300

    Adding polymorphism and selad class

---
+++ 26-polymorphism-sealed/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _26_polymorphism_sealed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Table table = new Table();
+            table.GetPrice();
+            
+        }
+    }
+
+    sealed class ExampleSealedClass
+    {
+        // this class cannot be inherited
+        // It is unnecessary to use modifiers such as protected and virtual
+    }
+
+    class Products{
+        public virtual void GetPrice(){
+            Console.WriteLine("Price Infos");
+        }
+    }
+
+    class Table:Products
+    {
+        // to use our virtual methods
+        // we need to call it with override
+
+        public override void GetPrice()
+        {
+            base.GetPrice();
+            Console.WriteLine("Overloading.....");
+        }
+
+    }
+}
